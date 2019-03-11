b630e7675c196f09bb2897930ffe32d5b5d5a874
Diane du Payrat <dianedupayrat@gmail.com>
2019-03-11 11:38:06 +0100

    Create routes and generate a new QuestionsController with 2 methods ask and answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
+  get 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/ask', to: 'questions#ask', as: :ask
+  get '/answer', to: 'questions#answer', as: :answer
 end
+
+# get 'about', to: 'pages#about', as: :about
+# get 'contact', to: 'pages#contact', as: :contact
