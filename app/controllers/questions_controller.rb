7214c9293322241aabd7a3200f67cd8f70828fe8
Diane du Payrat <dianedupayrat@gmail.com>
2019-03-11 12:34:19 +0100

    Implement the logic in the answer action in Controller and View

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,31 @@
 class QuestionsController < ApplicationController
+  # COACH_CASES = [
+  #   {
+  #     your_message: 'I am going to work',
+  #     coach_answer: 'Great!'
+  #   },
+  #   {
+  #     your_message: 'What time is it?',
+  #     coach_answer: 'Silly question, get dressed and go to work!'
+  #   },
+  #   {
+  #     your_message: 'hello',
+  #     coach_answer: 'I don\'t care, get dressed and go to work!'
+  #   }
+  # ]
+
   def ask
   end
 
   def answer
+    @your_message = params[:question]
+
+    if @your_message == 'I am going to work'
+      @coach_answer = 'Great!'
+    elsif @your_message.include? '?'
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
